c70f6170876486c3e92714366428063972037fe3
naufal03 <naufalarkan134@gmail.com>
2024-08-05 18:11:03 +0700

    page: company

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// ===============================================================================================================
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -21,6 +22,18 @@ Route::get('/about', function () {
     return Inertia::render('About/About');
 });
 
+
+
+// ====== My Routes ======
+Route::get('/company', function () {
+    return Inertia::render('Company',[
+        'title' => 'Company Page'
+    ]);
+});
+
+
+
+// ===============================================================================================================
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
