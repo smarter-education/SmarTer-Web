1898b02a399794e6be2e498aeb1b8f0b1416aae6
hanwil123 <farhanwildan12366@gmail.com>
2024-08-22 20:45:52 +0700

    second commit

--- resources/js/Pages/About/About.jsx
+++ resources/js/Pages/About/About.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div>
-        <h1 className=" bg-white text-xl">ABOUT</h1>
-    </div>
-  )
-}
+    return (
+        <nav>
+            <div className="bg-smarter7 h-[46px] flex justify-between">
+                <div className="flex justify-center items-center">
+                    <div>
+                        <ul className="flex gap-[33px] text-white">
+                            <li>Home</li>
+                            <li>Belajar</li>
+                            <li>Tentang Kami</li>
+                            <a href={route('logout')} method="post"><li>Logout</li></a>
+                        </ul>
+                    </div>
+                </div>
+                <div></div>
+            </div>
+        </nav>
+    );
+};
 
 export default About
