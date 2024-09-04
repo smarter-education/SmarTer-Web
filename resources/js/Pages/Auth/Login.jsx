820c417b671797db7f2c008b0a9630aa5774eb47
ashafaatadhis <skullreaper387@gmail.com>
2024-09-04 16:20:31 +0700

    feat: creating login and register page

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,98 +1,209 @@
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/auth/register/InputLabel";
 
-export default function Login({ status, canResetPassword }) {
+import TextInput from "@/Components/auth/register/TextInput";
+import { Head, Link, useForm, router } from "@inertiajs/react";
+import anime from "animejs";
+
+import "animate.css";
+import { useEffect } from "react";
+import Checkbox from "@/Components/Checkbox";
+import { Button } from "flowbite-react";
+export default function Login() {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
-        remember: false,
+        name: "",
+
+        password: "",
+        rememberme: false,
     });
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('login'), {
-            onFinish: () => reset('password'),
+        post(route("register"), {
+            onFinish: () => reset("password", "password_confirmation"),
         });
     };
-    const GoogleSubmit = async () => {
-        window.location.href = '/auth/redirect';
 
-    }
+    const handleAnimation = (e) => {
+        e.preventDefault();
+        anime
+            .timeline({
+                duration: 400, // Durasi animasi (dalam milidetik)
+                loop: false,
+                easing: "easeInOutQuad", // Efek transisi
+            })
+
+            .add({
+                targets: ".first",
+
+                right: "-50%",
+            })
+            .add({
+                targets: ".second",
+                delay: 400,
+                right: "-50%",
+            })
+            .add(
+                {
+                    targets: ".third",
+
+                    left: "-50%",
+                    complete: () => {
+                        // Setelah animasi selesai, arahkan ke halaman baru
+                        // router.visit("/auth/login");
+                    },
+                },
+                "-=400"
+            )
+            .add({
+                targets: ".fourth",
+                // delay: 400,
+                left: "-50%",
+                complete: () => {
+                    // Setelah animasi selesai, arahkan ke halaman baru
+                    router.visit("/register");
+                },
+            });
+    };
+
+    useEffect(() => {
+        anime
+            .timeline({
+                duration: 400, // Durasi animasi (dalam milidetik)
+                easing: "easeInOutQuad", // Efek transisi
+            })
+            .add({
+                targets: ".player ",
+                left: 128,
+            });
+    }, []);
 
     return (
-        <GuestLayout>
-            <Head title="Log in" />
+        <>
+            <Head title="Register" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+            <main className="bg-white min-h-screen relative overflow-x-hidden">
+                <img
+                    src="/auth/login/cloud-right.png"
+                    alt=""
+                    className="second absolute z-10 right-0 h-full top-0 animate__animated animate__slideInRight animate__delay-1s"
+                />
+                <img
+                    src="/auth/login/buble-left.png"
+                    alt=""
+                    className="first absolute z-10 right-0 h-full top-0 animate__animated animate__slideInRight"
+                />
+                <img
+                    src="/auth/login/cloud-left.png"
+                    alt=""
+                    className="third absolute z-10 left-0 h-full top-0 animate__animated animate__slideInLeft animate__delay-1s"
+                />
 
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
+                <section className="flex      text-smarter10  bg-black ">
+                    <div className="w-[526px] fourth player fixed top-10 z-20 -left-[50%] ">
+                        <h1 className="font-semibold text-7xl">Sign In</h1>
+                        <h2 className="font-normal mt-2 text-2xl">
+                            Welcome Back
+                        </h2>
+                        <p>
+                            Don’t have an account?{" "}
+                            <Link
+                                onClick={handleAnimation}
+                                className="cursor-pointer"
+                                href="/register"
+                            >
+                                Create a account
+                            </Link>
+                        </p>
+                        <p>It takes less than a minute.</p>
+                        <form onSubmit={submit}>
+                            <div className="mt-5">
+                                <InputLabel htmlFor="name" value="Name" />
+                                <TextInput
+                                    id="name"
+                                    name="name"
+                                    value={data.name}
+                                    className="mt-2 block max-w-[586px] w-full"
+                                    autoComplete="name"
+                                    isFocused={true}
+                                    onChange={(e) =>
+                                        setData("name", e.target.value)
+                                    }
+                                    required
+                                />
 
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
+                                <InputError
+                                    message={errors.name}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div className="mt-5">
+                                <InputLabel
+                                    htmlFor="password"
+                                    value="Password"
+                                />
+                                <TextInput
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    value={data.password}
+                                    className="mt-2 block max-w-[586px] w-full"
+                                    autoComplete="password"
+                                    isFocused={true}
+                                    onChange={(e) =>
+                                        setData("password", e.target.value)
+                                    }
+                                    required
+                                />
 
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="block mt-4">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
-                        />
-                        <span className="ms-2 text-sm text-gray-600">Remember me</span>
-                    </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
-            <button className="btn" onClick={GoogleSubmit}>Login With Google</button>
-
-        </GuestLayout>
+                                <InputError
+                                    message={errors.password}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div className="mt-5 flex items-center gap-2">
+                                <Checkbox
+                                    name={"rememberme"}
+                                    id="rememberme"
+                                    value={data.rememberme}
+                                    onChange={(e) =>
+                                        setData("rememberme", e.target.value)
+                                    }
+                                    className="rounded-full"
+                                />
+                                <InputLabel
+                                    htmlFor="rememberme"
+                                    className="text-base"
+                                    value="Remember Me"
+                                />
+                                <InputError
+                                    message={errors.name}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div className="mt-5">
+                                <button className="bg-[#6A5898] w-full text-white text-center rounded-[20px] p-3">
+                                    Sign In
+                                </button>
+                            </div>
+                            <div className="mt-5 flex gap-3 items-center">
+                                <div className="border  border-t-black w-full h-0" />
+                                <span>or</span>
+                                <div className="border  border-t-black w-full h-0" />
+                            </div>
+                            <div className="mt-5">
+                                <button className=" border border-black w-full   text-center rounded-[20px] p-3">
+                                    <img
+                                        src="/google-icon.png"
+                                        className="mx-auto"
+                                    />
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </section>
+            </main>
+        </>
     );
 }
--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,18 +1,20 @@
-import GuestLayout from "@/Layouts/GuestLayout";
 import InputError from "@/Components/InputError";
 import InputLabel from "@/Components/auth/register/InputLabel";
-import PrimaryButton from "@/Components/PrimaryButton";
+
 import TextInput from "@/Components/auth/register/TextInput";
-import { Head, Link, useForm } from "@inertiajs/react";
+import { Head, Link, useForm, router } from "@inertiajs/react";
 import anime from "animejs";
+
 import "animate.css";
 import { useEffect } from "react";
+import Checkbox from "@/Components/Checkbox";
+import { Button } from "flowbite-react";
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
         name: "",
-        email: "",
+
         password: "",
-        password_confirmation: "",
+        rememberme: false,
     });
 
     const submit = (e) => {
@@ -23,10 +25,52 @@ export default function Register() {
         });
     };
 
+    const handleAnimation = (e) => {
+        e.preventDefault();
+
+        anime
+            .timeline({
+                duration: 400, // Durasi animasi (dalam milidetik)
+                loop: false,
+                easing: "easeInOutQuad", // Efek transisi
+            })
+
+            .add({
+                targets: ".first",
+
+                left: "-50%",
+            })
+            .add({
+                targets: ".second",
+                delay: 400,
+                left: "-50%",
+            })
+            .add(
+                {
+                    targets: ".third",
+
+                    right: "-50%",
+                },
+                "-=400"
+            )
+            .add(
+                {
+                    targets: ".fourth",
+
+                    top: "-50%",
+                    complete: () => {
+                        // Setelah animasi selesai, arahkan ke halaman baru
+                        router.visit("/login");
+                    },
+                },
+                "-=400"
+            );
+    };
+
     useEffect(() => {
         anime
             .timeline({
-                duration: 1000, // Durasi animasi (dalam milidetik)
+                duration: 400, // Durasi animasi (dalam milidetik)
                 easing: "easeInOutQuad", // Efek transisi
             })
             .add({
@@ -46,38 +90,40 @@ export default function Register() {
             <main className="bg-white min-h-screen relative overflow-x-hidden">
                 <img
                     src="/auth/dot-up.png"
-                    className="absolute z-10 right-0 top-0 animate__animated animate__slideInDown"
+                    className="fourth absolute z-10 right-0 top-0 animate__animated animate__slideInDown"
                     alt=""
                 />
                 <img
                     src="/auth/left-cloud.png"
-                    className="absolute z-20 left-0 top-0 animate__animated animate__slideInLeft animate__delay-1s w-[40%] h-screen"
+                    className="first absolute z-20 opacity-100 transition-all left-0 top-0 animate__animated animate__slideInLeft animate__delay-1s w-[30%] h-screen"
                     alt=""
                 />
                 <img
                     src="/auth/dot-left.png"
-                    className="absolute z-10 left-0 h-screen animate__animated animate__slideInLeft"
+                    className="second transition-all absolute z-10 left-0 h-screen animate__animated animate__slideInLeft"
                     alt=""
                 />
                 <img
                     src="/auth/cloud-right-top.png"
                     alt=""
-                    className="absolute z-10 right-0 top-0 animate__animated h-[80%] animate__slideInRight animate__delay-1s"
+                    className="third absolute z-10 right-0 top-0 animate__animated h-[80%] animate__slideInRight animate__delay-1s"
                 />
                 <img
                     src="/auth/cloud-right-bot.png"
                     alt=""
-                    className="absolute z-10 right-0 bottom-0 animate__animated h-[40%] animate__slideInRight animate__delay-1s"
+                    className="third absolute z-10 right-0 bottom-0 animate__animated h-[40%] animate__slideInRight animate__delay-1s"
                 />
                 <section className="flex      text-smarter10   ">
-                    <div className="w-[586px] player fixed top-10 -right-[50%] ">
+                    <div className="w-[526px] third player fixed top-10 -right-[50%] ">
                         <h1 className="font-semibold text-7xl">Sign up</h1>
                         <h2 className="font-normal mt-2 text-2xl">
                             Welcome Back
                         </h2>
                         <p>
                             Have an account?{" "}
-                            <Link href="/auth/login">Sign in</Link>
+                            <Link onClick={handleAnimation} href="/login">
+                                Sign in
+                            </Link>
                         </p>
                         <p>It takes less than a minute.</p>
                         <form onSubmit={submit}>
@@ -102,25 +148,66 @@ export default function Register() {
                                 />
                             </div>
                             <div className="mt-5">
-                                <InputLabel htmlFor="name" value="Password" />
+                                <InputLabel
+                                    htmlFor="password"
+                                    value="Password"
+                                />
                                 <TextInput
-                                    id="name"
+                                    id="password"
                                     type="password"
-                                    name="name"
-                                    value={data.name}
+                                    name="password"
+                                    value={data.password}
                                     className="mt-2 block max-w-[586px] w-full"
-                                    autoComplete="name"
+                                    autoComplete="password"
                                     isFocused={true}
                                     onChange={(e) =>
-                                        setData("name", e.target.value)
+                                        setData("password", e.target.value)
                                     }
                                     required
                                 />
 
                                 <InputError
+                                    message={errors.password}
+                                    className="mt-2"
+                                />
+                            </div>
+                            <div className="mt-5 flex items-center gap-2">
+                                <Checkbox
+                                    name={"rememberme"}
+                                    id="rememberme"
+                                    value={data.rememberme}
+                                    onChange={(e) =>
+                                        setData("rememberme", e.target.value)
+                                    }
+                                    className="rounded-full"
+                                />
+                                <InputLabel
+                                    htmlFor="rememberme"
+                                    className="text-base"
+                                    value="Remember Me"
+                                />
+                                <InputError
                                     message={errors.name}
                                     className="mt-2"
                                 />
+                            </div>
+                            <div className="mt-5">
+                                <button className="bg-[#6A5898] w-full text-white text-center rounded-[20px] p-3">
+                                    Sign Up
+                                </button>
+                            </div>
+                            <div className="mt-5 flex gap-3 items-center">
+                                <div className="border  border-t-black w-full h-0" />
+                                <span>or</span>
+                                <div className="border  border-t-black w-full h-0" />
+                            </div>
+                            <div className="mt-5">
+                                <button className=" border border-black w-full   text-center rounded-[20px] p-3">
+                                    <img
+                                        src="/google-icon.png"
+                                        className="mx-auto"
+                                    />
+                                </button>
                             </div>
                         </form>
                     </div>
