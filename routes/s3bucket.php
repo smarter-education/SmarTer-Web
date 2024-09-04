4ebf04ef1968f83973e844e1b394694e3e26cc5e
hanwil123 <farhanwildan12366@gmail.com>
2024-09-04 17:22:51 +0700

    adjust router s3bucket and crsf too

--- routes/s3bucket.php
+++ routes/s3bucket.php
@@ -3,13 +3,11 @@
 use App\Http\Controllers\S3BucketBackblazeController;
 use Illuminate\Support\Facades\Route;
 
-    Route::post('/folder/file/upload-file', [S3BucketBackblazeController::class, 'uploadFile']);
-    Route::delete('/folder/file/delete-file', [S3BucketBackblazeController::class, 'deleteFile']);
-    Route::get('/folder/file/list-files', [S3BucketBackblazeController::class, 'listFiles']);
-    Route::get('/folder/file/download-file', [S3BucketBackblazeController::class, 'downloadFile']);
+Route::prefix('s3bucket')->group(function () {
+    Route::post('/file/upload-file', [S3BucketBackblazeController::class, 'uploadFile']);
+    Route::delete('/file/delete-file', [S3BucketBackblazeController::class, 'deleteFile']);
+    Route::get('/file/list-files', [S3BucketBackblazeController::class, 'listFiles']);
+    Route::get('/file/download-file', [S3BucketBackblazeController::class, 'downloadFile']);
     Route::delete('/folder/delete-folder', [S3BucketBackblazeController::class, 'deleteFolder']);
     Route::get('/folder/list-folders', [S3BucketBackblazeController::class, 'listFolders']);
-    Route::get('/folder/download-folder', [S3BucketBackblazeController::class, 'downloadFolder']);
-    Route::post('/folder/upload-folder', [S3BucketBackblazeController::class, 'uploadFolder']);
-    Route::delete('/folder/delete-folder-content', [S3BucketBackblazeController::class, 'deleteFolderContent']);
-    Route::get('/folder/download-folder-content', [S3BucketBackblazeController::class, 'downloadFolderContent']);
+});
