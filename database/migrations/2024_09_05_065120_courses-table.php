db4c41f6d5267b008045f6988a82f9b845e4edfa
hanwil123 <farhanwildan12366@gmail.com>
2024-09-05 13:57:32 +0700

    add schema database for courses, subscription and payment without migrating

---
+++ database/migrations/2024_09_05_065120_courses-table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');  // Judul pelajaran seperti Matematika, Biologi, dll.
+            $table->text('description');
+            $table->string('category');  // Contoh: SMA, Matematika, dll.
+            $table->string('type');  // Tipe konten: video, document
+            $table->string('file_path');  // Lokasi file video atau dokumen di penyimpanan
+            $table->boolean('is_paid')->default(true);  // Apakah konten berbayar?
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
