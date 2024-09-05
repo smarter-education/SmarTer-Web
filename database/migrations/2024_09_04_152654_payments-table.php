7d30796790e27c38a5b463fb14e20996a9f502d0
hanwil123 <farhanwildan12366@gmail.com>
2024-09-05 13:57:32 +0700

    add schema database for courses, subscription and payment without migrating

--- database/migrations/2024_09_04_152654_payments-table.php
+++ database/migrations/2024_09_04_152654_payments-table.php
@@ -11,8 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
-    }
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('subscription_id')->nullable()->constrained('subscriptions')->onDelete('cascade');
+            $table->foreignId('courses_id')->nullable()->constrained('courses')->onDelete('cascade');
+            $table->enum('payment_type', ['subscription', 'unlock'])->default('subscription');
+            $table->decimal('amount', 10, 2);
+            $table->enum('status', ['completed', 'failed', 'refunded'])->default('completed');
+            $table->string('payment_method');
+            $table->timestamps();
+        });    }
 
     /**
      * Reverse the migrations.
