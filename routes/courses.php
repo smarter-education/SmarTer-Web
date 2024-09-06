ab30e8c72bbda6eef4e1f4e2d2b7111b615128cf
hanwil123 <farhanwildan12366@gmail.com>
2024-09-06 22:29:47 +0700

    add controller for get all, add, getbyId and delete Course and add router for that controller

--- routes/courses.php
+++ routes/courses.php
@@ -6,4 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('courses')->group(function () {
     Route::get('/get-courses', [CourseController::class, 'getAllCourses']);
     Route::post('/add-courses', [CourseController::class, 'addCourses']);
+    Route::delete('/delete-courses/{id}', [CourseController::class, 'deleteCourse']);
+    Route::get('/get-courses/{id}', [CourseController::class, 'getCourseById']);
 });
+
