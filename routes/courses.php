4afa38dcd201047f440540eb4632dc145d5692ae
hanwil123 <farhanwildan12366@gmail.com>
2024-09-06 22:48:48 +0700

    add controller and router for update course

--- routes/courses.php
+++ routes/courses.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('courses')->group(function () {
-    Route::get('/get-courses', [CourseController::class, 'getAllCourses']);
     Route::post('/add-courses', [CourseController::class, 'addCourses']);
-    Route::delete('/delete-courses/{id}', [CourseController::class, 'deleteCourse']);
+    Route::get('/get-courses', [CourseController::class, 'getAllCourses']);
     Route::get('/get-courses/{id}', [CourseController::class, 'getCourseById']);
+    Route::put('/update-courses/{id}', [CourseController::class, 'updateCourses']);
+    Route::delete('/delete-courses/{id}', [CourseController::class, 'deleteCourse']);
 });
 
