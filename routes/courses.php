9c6215fa5536a6e8e883b1ee06c723c602986cf5
ashafaatadhis <skullreaper387@gmail.com>
2024-09-07 21:04:35 +0700

    conflict

--- routes/courses.php
+++ routes/courses.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('courses')->group(function () {
 <<<<<<< HEAD
+<<<<<<< HEAD
     Route::post('/add-courses', [CourseController::class, 'addCourses']);
     Route::get('/get-courses', [CourseController::class, 'getAllCourses']);
     Route::get('/get-courses/{id}', [CourseController::class, 'getCourseById']);
@@ -14,9 +15,13 @@ Route::prefix('courses')->group(function () {
 
 =======
     Route::get('/get-courses', [CourseController::class, 'getAllCourses']);
+=======
+>>>>>>> c7a5551 (add controller and router for update course)
     Route::post('/add-courses', [CourseController::class, 'addCourses']);
-    Route::delete('/delete-courses/{id}', [CourseController::class, 'deleteCourse']);
+    Route::get('/get-courses', [CourseController::class, 'getAllCourses']);
     Route::get('/get-courses/{id}', [CourseController::class, 'getCourseById']);
+    Route::put('/update-courses/{id}', [CourseController::class, 'updateCourses']);
+    Route::delete('/delete-courses/{id}', [CourseController::class, 'deleteCourse']);
 });
 <<<<<<< HEAD
 >>>>>>> e66ad5a (add router for courses and adjust in schema table payments and course controllers)
