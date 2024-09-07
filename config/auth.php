496ba84d1b033381e77128b25a98dd212befba45
ashafaatadhis <skullreaper387@gmail.com>
2024-09-07 20:54:10 +0700

    fix conflict

--- config/auth.php
+++ config/auth.php
@@ -14,8 +14,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'guard' => 'google',
+        'passwords' => 'users-google',
     ],
 
     /*
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'google' => [
+            'driver' => 'jwt',
+            'provider' => 'users-google',
+        ]
     ],
 
     /*
